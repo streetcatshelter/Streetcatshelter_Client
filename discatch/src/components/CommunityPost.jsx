ee3406fc96d11113aba7d754f38eb7d3277d3887
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-09-08 21:14:23 +0900

    🔨 Image 수정, 커뮤니티 중간 작업

--- discatch/src/components/CommunityPost.jsx
+++ discatch/src/components/CommunityPost.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+// ELEMENTS
+import { Grid } from '../elements/index';
+
+// STYLE
+import styled, { css } from 'styled-components';
+
 const CommunityPost = () => {
-  return <div></div>;
+  return (
+    <Grid height="30px">
+      <CommunityPostStyle>
+        9월 8일 (수) 18:00 망원 2동 순찰돕니다.
+      </CommunityPostStyle>
+    </Grid>
+  );
 };
+
+const CommunityPostStyle = styled.div`
+  border-bottom: 2px solid rgb(${(props) => props.theme.palette.olive});
+  margin: 10px;
+`;
 
 export default CommunityPost;
--- discatch/src/pages/Community.jsx
+++ discatch/src/pages/Community.jsx
@@ -1,10 +1,16 @@
+// LIBRARY
 import React from 'react';
 
+// COMPONENTS
 import CommunityCategoryCard from '../components/CommunityCategoryCard'
+
+// ELEMENTS
+import { Grid } from '../elements/index';
 
 const Community = () => {
   return (
   <>
+  <Grid margin="20vh 0 0 0">
     <CommunityCategoryCard 
     width='60px' 
     height='60px'
@@ -17,6 +23,7 @@ const Community = () => {
     width='60px' 
     height='60px'
     />
+    </Grid>
   </>
   )
 };

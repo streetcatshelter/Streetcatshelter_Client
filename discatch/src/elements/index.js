171b4cb3c4beb79f68cf0fc03bcb4ba9c9e43721
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-09-08 21:31:13 +0900

    :wrench: elements index 추가

---
+++ discatch/src/elements/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Grid from "./Grid";
+import Input from "./Input";
+import Text from "./Text";
+import TextArea from "./TextArea";
+import Image from "./Image";
+
+export { Button, Grid, Input, Text, TextArea, Image };

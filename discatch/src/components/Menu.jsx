7f9ef424b998e781d7166a7d63023acd510f567f
sunm-in <sunm309@naver.com>
2021-09-08 17:35:04 +0900

    💄 Menu components update

--- discatch/src/components/Menu.jsx
+++ discatch/src/components/Menu.jsx
@@ -1,7 +1,131 @@
+// library
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+// route
+import { Link } from 'react-router-dom';
+
+// element
+import { Grid, Text } from '../elements';
+
+// icon
+import { Home, Users, Compass, Send, User } from 'react-feather';
+import { flexBox } from '../shared/style';
 
 const Menu = () => {
-  return <div></div>;
+  return (
+    <MenuStyle>
+      <Grid
+        addstyle={() => {
+          return css`
+            ${flexBox('space-around')}
+          `;
+        }}
+      >
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+            fontSize: '10px',
+            textAlign: 'center',
+          }}
+        >
+          <Home />
+          <p
+            style={{
+              margin: '0',
+            }}
+          >
+            홈
+          </p>
+        </Link>
+        <Link
+          to="/community"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+            fontSize: '10px',
+            textAlign: 'center',
+          }}
+        >
+          <Users />
+          <p
+            style={{
+              margin: '0',
+            }}
+          >
+            커뮤니티
+          </p>
+        </Link>
+        <Link
+          to="/map"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+            fontSize: '10px',
+            textAlign: 'center',
+          }}
+        >
+          <Compass />
+          <p
+            style={{
+              margin: '0',
+            }}
+          >
+            지도
+          </p>
+        </Link>
+        <Link
+          to="/chat"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+            fontSize: '10px',
+            textAlign: 'center',
+          }}
+        >
+          <Send />
+          <p
+            style={{
+              margin: '0',
+            }}
+          >
+            채팅
+          </p>
+        </Link>
+        <Link
+          to="/mypage"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+            fontSize: '10px',
+            textAlign: 'center',
+          }}
+        >
+          <User />
+          <p
+            style={{
+              margin: '0',
+            }}
+          >
+            내정보
+          </p>
+        </Link>
+      </Grid>
+    </MenuStyle>
+  );
 };
+
+const MenuStyle = styled.footer`
+  width: 100%;
+  height: 50px;
+  background: #fbd986;
+  display: flex;
+  justify-content: space-around;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+`;
 
 export default Menu;

efc91a17e7f35c9253fae8fa1e9767e4c10a8ec6
sunm-in <sunm309@naver.com>
2021-09-09 03:31:46 +0900

    💄 Header components update

--- discatch/src/components/Header.jsx
+++ discatch/src/components/Header.jsx
@@ -1,6 +1,7 @@
 // library
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled, { css } from 'styled-components';
+import Select from 'react-select';
 
 // style
 import { flexBox } from '../shared/style';
@@ -15,11 +16,46 @@ import { Grid, Text } from '../elements';
 import { Search, Bell } from 'react-feather';
 
 const Header = () => {
+  const options = useMemo(
+    () => [
+      { value: '', label: '' },
+      { value: '망원동', label: '망원동' },
+      { value: '고척동', label: '고척동' },
+      { value: '개봉동', label: '개봉동' },
+    ],
+    [],
+  );
+
   return (
     <HeaderStyle>
-      <SelectStyle />
-      <Search />
-      <Bell />
+      <Grid
+        addstyle={() => {
+          return css`
+            ${flexBox('space-between')}
+          `;
+        }}
+      >
+        <CustomSelect placeholder="지역" options={options} />
+
+        <Link to="/" style={{ margin: '0 auto', textDecoration: 'none' }}>
+          <Text
+            color="D_olive"
+            size="26px"
+            fontWeight="600"
+            addstyle={() => {
+              return css`
+                font-family: 'Walter Turncoat';
+                /* font-family: 'Nunito', sans-serif; */
+              `;
+            }}
+          >
+            disCATch
+          </Text>
+        </Link>
+
+        <Search style={{ color: 'gray' }} />
+        <Bell style={{ margin: '0 4% 0 3%', color: 'gray' }} />
+      </Grid>
     </HeaderStyle>
   );
 };
@@ -27,18 +63,33 @@ const Header = () => {
 const HeaderStyle = styled.header`
   width: 100%;
   height: 60px;
-  background: olive;
+  background: white;
   position: fixed;
   top: 0;
   left: 0;
   z-index: 9;
+  display: flex;
 `;
 
-const SelectStyle = styled.select`
-  width: 50px;
-  height: 30px;
-  background: white;
-  outline: none;
+const CustomSelect = styled(Select)`
+  width: 95px;
+  margin-left: 3%;
+  margin-right: -6%;
+
+  // selectBox
+  .css-yk16xz-control {
+    background: none;
+    border: none;
+  }
+
+  // placeholder
+  .css-1wa3eu0-placeholder {
+    color: lightgray;
+  }
+
+  // arrow
+  .css-tlfecz-indicatorContainer {
+  }
 `;
 
 export default Header;

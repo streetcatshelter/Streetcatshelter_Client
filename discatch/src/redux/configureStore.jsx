2a5b17d56ba59e30aae25df6d3e482e5b033ae9e
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-09-08 21:33:41 +0900

    :wrench: configureStore 수정

--- discatch/src/redux/configureStore.jsx
+++ discatch/src/redux/configureStore.jsx
@@ -1,27 +1,27 @@
-// // library
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// library
+import { createStore, combineReducers, applyMiddleware } from "redux";
 
-// // middleware
-// import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
+// middleware
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
-// // redux router
-// import { createBrowserHistory } from 'history';
-// import { connectRouter } from 'connected-react-router';
+// redux router
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
 
-// // reducer
+// reducer
 
-// const history = createBrowserHistory();
+const history = createBrowserHistory();
 
-// const rootReducer = combineReducers({
-//   router: connectRouter(history),
-// });
+const rootReducer = combineReducers({
+  router: connectRouter(history),
+});
 
-// // history 넣기, 로거사용
-// const middleware = [thunk.withExtraArgument({ history }), logger];
+// history 넣기, 로거사용
+const middleware = [thunk.withExtraArgument({ history }), logger];
 
-// // 미들웨어와 리듀서 묶어서 store생성
-// const store = createStore(rootReducer, applyMiddleware(...middleware));
-// export { history };
+// 미들웨어와 리듀서 묶어서 store생성
+const store = createStore(rootReducer, applyMiddleware(...middleware));
+export { history };
 
-// export default store;
+export default store;

b25ddeef854b54a63aa78381655275143049747d
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-09-08 16:42:21 +0900

    💄 Button elements update

--- discatch/src/elements/Button.jsx
+++ discatch/src/elements/Button.jsx
@@ -1,7 +1,38 @@
+// LIBRARY
 import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => {
-  return <div></div>;
+// STYLE
+import { borderBox } from '../shared/style';
+
+const Button = ({ children, clickEvent, ...props }) => {
+  return (
+    <ButtonStyle onClick={clickEvent} {...props}>
+      {children}
+    </ButtonStyle>
+  );
 };
+
+Button.defaultProps = {
+  color: 'black',
+  bgColor: 'white',
+  fontSize: '14px',
+  padding: '12px 0px',
+  addstyle: () => {},
+  clickEvent: () => {},
+};
+
+const ButtonStyle = styled.button`
+  width: ${(props) => props.width};
+  background: ${(props) => `rgb(${props.theme.palette[props.bgColor]})`};
+  color: ${(props) => `rgb(${props.theme.palette[props.color]})`};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  margin: ${(props) => props.margin};
+  border: none;
+  cursor: pointer;
+  ${(props) => borderBox(props.radius, props.padding)}
+  ${(props) => props.addstyle()};
+`;
 
 export default Button;

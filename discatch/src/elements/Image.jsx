5bcb2f057f13280ba77f66f7dc93d41625ed917f
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-09-08 21:32:23 +0900

    :wrench: element 수정
    :wrench: element 수정

--- discatch/src/elements/Image.jsx
+++ discatch/src/elements/Image.jsx
@@ -1,37 +1,30 @@
-import React from 'react';
+import React from "react";
 
 // STYLE
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Image = ({ src, width, height, ...props }) => {
   return (
     <React.Fragment>
-      <AspectOutter>
-        <AspectInner
-          {...props}
-          src={src}
-          width={width}
-          height={height}
-        ></AspectInner>
-      </AspectOutter>
+      <AspectInner
+        {...props}
+        src={src}
+        width={width}
+        height={height}
+      ></AspectInner>
     </React.Fragment>
   );
 };
 Image.defaultProps = {
-  src: 'https://cdn.pixabay.com/photo/2021/08/10/18/32/cat-6536684_1280.jpg',
-  width: '50px',
-  height: '50px',
+  src: "https://cdn.pixabay.com/photo/2021/08/10/18/32/cat-6536684_1280.jpg",
+  width: "50px",
+  height: "50px",
   addstyle: () => {},
 };
 
-const AspectOutter = styled.div`
-  width: 100%;
-  min-width: 250px;
-`;
-
 const AspectInner = styled.div`
   position: relative;
-  background-image: url('${(props) => props.src}');
+  background-image: url("${(props) => props.src}");
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   background-position: center;

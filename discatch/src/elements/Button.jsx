abf99c64fb861ceb36523a63750f2be7115bab86
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-09-10 00:29:15 +0900

    :wrench: 버튼, 그리드 element 수정 및 추가

--- discatch/src/elements/Button.jsx
+++ discatch/src/elements/Button.jsx
@@ -1,11 +1,18 @@
 // LIBRARY
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 // STYLE
-import { borderBox } from '../shared/style';
+import { borderBox } from "../shared/style";
 
-const Button = ({ children, clickEvent, ...props }) => {
+const Button = ({ is_float, children, clickEvent, ...props }) => {
+  if (is_float) {
+    return (
+      <React.Fragment>
+        <FloatButton onClick={clickEvent}>{children}</FloatButton>
+      </React.Fragment>
+    );
+  }
   return (
     <ButtonStyle onClick={clickEvent} {...props}>
       {children}
@@ -14,10 +21,10 @@ const Button = ({ children, clickEvent, ...props }) => {
 };
 
 Button.defaultProps = {
-  color: 'black',
-  bgColor: 'white',
-  fontSize: '14px',
-  padding: '12px 0px',
+  color: "black",
+  bgColor: "white",
+  fontSize: "14px",
+  padding: "12px 0px",
   addstyle: () => {},
   clickEvent: () => {},
 };
@@ -35,4 +42,21 @@ const ButtonStyle = styled.button`
   ${(props) => props.addstyle()};
 `;
 
+const FloatButton = styled.button`
+  width: 45px;
+  height: 45px;
+  background-color: #b5bb19;
+  color: #ffffff;
+  box-sizing: border-box;
+  font-size: 36px;
+  font-weight: 700;
+  position: fixed;
+  bottom: 60px;
+  right: 16px;
+  text-align: center;
+  vertical-align: middle;
+  border: none;
+  border-radius: 22.5px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+`;
 export default Button;

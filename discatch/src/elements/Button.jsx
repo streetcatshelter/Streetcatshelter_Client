afe3f0de966d0ac9a0074ba6311a2804f8074040
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-09-17 00:01:01 +0900

    :lipstick: floatingbtn 위치 조정

--- discatch/src/elements/Button.jsx
+++ discatch/src/elements/Button.jsx
@@ -53,13 +53,12 @@ const FloatButton = styled.button`
   font-weight: 700;
   position: fixed;
   bottom: 60px;
-  left: 350px;
+  margin-left: 350px;
   text-align: center;
   vertical-align: middle;
   border: none;
   border-radius: 22.5px;
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-
   @media (max-width: 420px) {
     left: auto;
     right: 20px;

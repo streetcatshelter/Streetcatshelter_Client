5bc81341203158b192f3aac1182725c30b733a71
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-09-18 00:12:04 +0900

    Revert "Merge branch 'main' into jinwoo"
    This reverts commit 7a10d4a7bdfe585e78608c4ee7af651d327f78c0, reversing
    changes made to f38df2c97f91b681f6c9cd1cf9cbe48ee00c43a7.

--- discatch/src/components/catPosts/Diary.jsx
+++ discatch/src/components/catPosts/Diary.jsx
@@ -10,93 +10,88 @@ import { flexBox } from '../../shared/style';
 import { Eye, MessageCircle } from 'react-feather';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
-// route
-import { Link } from 'react-router-dom';
-
 const Diary = () => {
   return (
-    <Link to="catdetailinfo" style={{ textDecoration: 'none' }}>
+    <Grid
+      bgColor="diaryColor"
+      padding="6px"
+      width="95%"
+      margin="0 auto 2% auto"
+    >
       <Grid
-        bgColor="diaryColor"
-        padding="6px"
-        width="95%"
-        margin="0 auto 2% auto"
+        margin="0 0 3% 0"
+        display="flex"
+        alignItems="center"
+        addstyle={() => {
+          return css`
+            ${flexBox('space-between')}
+          `;
+        }}
       >
-        <Grid
-          margin="0 0 3% 0"
-          display="flex"
-          alignItems="center"
+        <Image width="30px" height="30px" margin="0 3% 0 0" />
+        <Text margin="0 30% 0 -35%">뽀삐맘</Text>
+        <Text
+          margin="0 3% 0 0"
           addstyle={() => {
             return css`
-              ${flexBox('space-between')}
+              font-size: 12px;
             `;
           }}
         >
-          <Image width="30px" height="30px" margin="0 3% 0 0" />
-          <Text margin="0 30% 0 -35%">뽀삐맘</Text>
-          <Text
-            margin="0 3% 0 0"
-            addstyle={() => {
-              return css`
-                font-size: 12px;
-              `;
-            }}
-          >
-            몇초전
-          </Text>
-        </Grid>
-
-        <Text>
-          오늘 간식줬어요~ 날씨 너무 좋은데 뽀삐 얼굴봐서 좋았습니다. 점점
-          마음을 여는것 같아서 너무 기쁘네요~ :)내일도 볼수있기를 바랍니다 !
+          몇초전
         </Text>
+      </Grid>
 
-        <Grid
-          bgcolor="diaryColor"
+      <Text>
+        오늘 간식줬어요~ 날씨 너무 좋은데 뽀삐 얼굴봐서 좋았습니다. 점점 마음을
+        여는것 같아서 너무 기쁘네요~ :)내일도 볼수있기를 바랍니다 !
+      </Text>
+
+      <Grid
+        bgcolor="diaryColor"
+        addstyle={() => {
+          return css`
+            ${flexBox('flex-end')}
+          `;
+        }}
+      >
+        <Button
+          margin="0 3%"
           addstyle={() => {
             return css`
-              ${flexBox('flex-end')}
+              background: #fcf6e8;
             `;
           }}
         >
-          <Button
-            margin="0 3%"
-            addstyle={() => {
-              return css`
-                background: #fcf6e8;
-              `;
-            }}
-          >
-            <Eye size="18" />
-            10
-          </Button>
+          <Eye size="18" />
+          10
+        </Button>
 
-          <Button
-            margin="0 3%"
-            addstyle={() => {
-              return css`
-                background: #fcf6e8;
-              `;
-            }}
-          >
-            <MessageCircle size="18" />
-            10
-          </Button>
+        <Button
+          margin="0 3%"
+          addstyle={() => {
+            return css`
+              background: #fcf6e8;
+            `;
+          }}
+        >
+          <MessageCircle size="18" />
+          10
+        </Button>
 
-          <Button
-            margin="0 3%"
-            addstyle={() => {
-              return css`
-                background: #fcf6e8;
-              `;
-            }}
-          >
-            <FavoriteIcon style={{ fontSize: '18', color: 'red' }} />
-            10
-          </Button>
-        </Grid>
+        <Button
+          margin="0 3%"
+          addstyle={() => {
+            return css`
+              background: #fcf6e8;
+            `;
+          }}
+        >
+          <FavoriteIcon style={{ fontSize: '18', color: 'red' }} />
+          10
+        </Button>
       </Grid>
-    </Link>
+    </Grid>
   );
 };
 

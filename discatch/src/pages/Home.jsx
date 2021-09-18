25ad5bab32cb0e9c4e931a06162e477cb2dae6ba
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-09-18 09:39:41 +0900

    Revert "Revert "Revert "Revert "Merge branch 'main' into jinwoo""""
    This reverts commit 1b6ccbb71ebc57635b7dea9d67f8473e6a568dd3.

--- discatch/src/pages/Home.jsx
+++ discatch/src/pages/Home.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+// library
+import React from 'react';
 
-/* == components*/
-import { CatPostList, Template } from "../components";
+// component
+import { CatPostList, Template } from '../components';
+
+// element
+import { Button } from '../elements';
+
+// icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+
+// redux
+import { history } from '../redux/configureStore';
 
 const Home = (props) => {
   return (
     <Template props={props}>
       <CatPostList />
+      <Button
+        is_float="is_float"
+        clickEvent={() => {
+          history.push('/catinfowrite');
+        }}
+      >
+        <FontAwesomeIcon icon={faPencilAlt} style={{ width: '20px' }} />
+      </Button>
     </Template>
   );
 };

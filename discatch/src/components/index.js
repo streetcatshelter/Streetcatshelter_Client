a323a9b92f985284c8c8d479db5232e0047206d5
sunm-in <sunm309@naver.com>
2021-09-20 13:26:40 +0900

    🔨 component - EditModal import 추가

--- discatch/src/components/index.js
+++ discatch/src/components/index.js
@@ -1,51 +1,52 @@
 /* Templates */
-import Router from "./Router";
-import Header from "./Header";
-import Menu from "./Menu";
-import Template from "./Template";
+import Router from './Router';
+import Header from './Header';
+import Menu from './Menu';
+import Template from './Template';
 
 /* ==Slider */
-import Slide from "./slider/Slide";
+import Slide from './slider/Slide';
 
 /* ==myPages */
-import MyPageCat from "./myPage/MyPageCat";
-import Notice from "./myPage/Notice";
-import Profile from "./myPage/Profile";
-import ProfileEdit from "./myPage/ProfileEdit";
-import ProgressBar from "./myPage/ProgressBar";
-import UserInfo from "./myPage/UserInfo";
-import MyPageDetail from "./myPage/MyPageDetail";
-import MyWork from "./myPage/MyWork";
-import MyPageContent from "./myPage/MyPageContent";
+import MyPageCat from './myPage/MyPageCat';
+import Notice from './myPage/Notice';
+import Profile from './myPage/Profile';
+import ProfileEdit from './myPage/ProfileEdit';
+import ProgressBar from './myPage/ProgressBar';
+import UserInfo from './myPage/UserInfo';
+import MyPageDetail from './myPage/MyPageDetail';
+import MyWork from './myPage/MyWork';
+import MyPageContent from './myPage/MyPageContent';
 
 /*==calendar */
-import Calendar from "./calendar/Calendar";
-import CalendarBody from "./calendar/CalendarBody";
-import CalendarHead from "./calendar/CalendarHead";
-import CalendarDates from "./calendar/CalendarDates";
-import CalendarModal from "./calendar/CalendarModal";
+import Calendar from './calendar/Calendar';
+import CalendarBody from './calendar/CalendarBody';
+import CalendarHead from './calendar/CalendarHead';
+import CalendarDates from './calendar/CalendarDates';
+import CalendarModal from './calendar/CalendarModal';
 
 /* catposts */
-import CatPost from "./catPosts/CatPost";
-import CatPostList from "./catPosts/CatPostList";
-import CatPostDetail from "./catPosts/CatPostDetail";
-import CatCalendar from "./catPosts/CatCalendar";
-import CatDiary from "./catPosts/CatDiary";
-import CatGallery from "./catPosts/CatGallery";
-import Diary from "./catPosts/Diary";
-import Gallery from "./catPosts/Gallery";
+import CatPost from './catPosts/CatPost';
+import CatPostList from './catPosts/CatPostList';
+import CatPostDetail from './catPosts/CatPostDetail';
+import CatCalendar from './catPosts/CatCalendar';
+import CatDiary from './catPosts/CatDiary';
+import CatGallery from './catPosts/CatGallery';
+import Diary from './catPosts/Diary';
+import Gallery from './catPosts/Gallery';
+import EditModal from './catPosts/EditModal';
 
 /* comments */
-import Comment from "./comment/Comment";
-import CommentList from "./comment/CommentList";
+import Comment from './comment/Comment';
+import CommentList from './comment/CommentList';
 
 /* community */
-import CommunityCategoryCard from "./community/CommunityCategoryCard";
-import CommunityPost from "./community/CommunityPost";
-import EditDeleteModal from "./community/EditDeleteModal";
+import CommunityCategoryCard from './community/CommunityCategoryCard';
+import CommunityPost from './community/CommunityPost';
+import EditDeleteModal from './community/EditDeleteModal';
 
 /* map */
-import Location from "./map/Location";
+import Location from './map/Location';
 
 export {
   /* ==Templates */
@@ -80,6 +81,7 @@ export {
   CatGallery,
   Diary,
   Gallery,
+  EditModal,
   /* ==comments */
   Comment,
   CommentList,

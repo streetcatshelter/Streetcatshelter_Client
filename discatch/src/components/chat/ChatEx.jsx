6e521b8c226783cb124385d62bf79a4dfd3c1682
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-11-15 21:43:07 +0900

    🔨 채팅 웹소켓 연결

--- discatch/src/components/chat/ChatEx.jsx
+++ discatch/src/components/chat/ChatEx.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as StompJs from "@stomp/stompjs";
 import * as SockJS from "sockjs-client";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { chatActions } from "../../redux/modules/chat";
-const ChatEx = (roomId) => {
+import styled from "styled-components";
+
+import { Send } from "react-feather";
+
+const ChatEx = (props) => {
   const dispatch = useDispatch();
   const ROOM_SEQ = 1;
   const token = localStorage.getItem("token");
+  const username = useSelector((state) => state.mypage.userInfo.username);
   const client = useRef({});
-  const [chatMessages, setChatMessages] = useState([]);
+
   const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    dispatch(chatActions._getAllMessage(roomId));
-  }, []);
-
+  const [chatMessages, setChatMessages] = useState([]);
+  console.log(chatMessages);
   useEffect(() => {
     connect();
 
@@ -25,7 +28,7 @@ const ChatEx = (roomId) => {
       // brokerURL: "ws://localhost:8080/ws-stomp/websocket", // 웹소켓 서버로 직접 접속
       webSocketFactory: () => new SockJS("http://52.78.241.50/ws-stomp"), // proxy를 통한 접속
       connectHeaders: {
-        token,
+        token: token,
       },
       debug: function (str) {
         console.log(str);
@@ -49,7 +52,7 @@ const ChatEx = (roomId) => {
   };
 
   const subscribe = () => {
-    client.current.subscribe(`/sub/chat/${ROOM_SEQ}`, ({ body }) => {
+    client.current.subscribe(`/sub/chat/${props.roomId}`, ({ body }) => {
       setChatMessages((_chatMessages) => [..._chatMessages, JSON.parse(body)]);
     });
   };
@@ -60,33 +63,72 @@ const ChatEx = (roomId) => {
     }
 
     client.current.publish({
-      destination: "/chat/message",
-      body: JSON.stringify({ roomSeq: ROOM_SEQ, message }),
+      destination: "/pub/api/chat/message",
+      headers: { token: token },
+      body: JSON.stringify({
+        message: message,
+        roomId: props.roomId,
+        userName: username,
+      }),
     });
 
     setMessage("");
   };
 
   return (
-    <div>
-      {chatMessages && chatMessages.length > 0 && (
+    <ChatSendBox>
+      {/* {chatMessages && chatMessages.length > 0 && (
         <ul>
           {chatMessages.map((_chatMessage, index) => (
             <li key={index}>{_chatMessage.message}</li>
           ))}
         </ul>
-      )}
-      <div>
-        <input
-          type={"text"}
-          placeholder={"message"}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyPress={(e) => e.which === 13 && publish(message)}
-        />
-        <button onClick={() => publish(message)}>send</button>
-      </div>
-    </div>
+      )} */}
+
+      <ChatInput
+        type={"text"}
+        placeholder={"message"}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(e) => e.which === 13 && publish(message)}
+      />
+      <SendButton onClick={() => publish(message)}>
+        <Send />
+      </SendButton>
+    </ChatSendBox>
   );
 };
+const ChatSendBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 100%;
+  height: 80px;
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+  overflow: auto;
+`;
+const ChatInput = styled.input`
+  display: flex;
+  width: 80%;
+  height: 50px;
+  padding: 10px;
+  background-color: #ffffff;
+  border: 1px solid #cbcf52;
+  border-radius: 10px;
+  resize: none;
+`;
+
+const SendButton = styled.div`
+  display: flex;
+  width: 20%;
+  svg {
+    width: 25px;
+    height: 25px;
+    margin-bottom: 10px;
+    stroke: #cbcf52;
+  }
+`;
+
 export default ChatEx;
--- discatch/src/components/chat/ChatTest.jsx
+++ discatch/src/components/chat/ChatTest.jsx
@@ -1,72 +1,140 @@
-import React, { useState } from "react";
-import "./ChatTest.css";
+import React, { useEffect } from "react";
+
 import { Image } from "../../elements";
 import { useDispatch, useSelector } from "react-redux";
-import { setMessage } from "../../redux/modules/chat";
-import { Send } from "react-feather";
-const ChatTest = () => {
-  const ChatMessage = useSelector((state) => state.chat.chatlist);
+import { chatActions } from "../../redux/modules/chat";
+import styled from "styled-components";
+
+const ChatTest = (props) => {
   const dispatch = useDispatch();
-  const [message, SetMessage] = useState("");
-
-  const ChangeMessage = (e) => {
-    SetMessage(e.target.value);
-  };
-
-  const SendMessage = () => {
-    if (message === " ") {
-      window.alert("메세지를 입력해주세요!");
-      return;
-    }
-    dispatch(setMessage(message));
-    SetMessage(" ");
-  };
-
-  const SendMessageByEnter = (e) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      SendMessage();
-      e.preventDefault();
-    }
-  };
+  const ChatInfo = useSelector((state) => state.chat.chatinfo);
+  const NickName = useSelector((state) => state.mypage.userInfo.nickname);
+  console.log(NickName);
+  useEffect(() => {
+    dispatch(chatActions._getRoomInfo(props.roomId));
+  }, []);
 
   return (
-    <div class="container">
-      <div class="header">
+    <ChatWrap>
+      <Header>
         {/* <button class="back-btn">
             <img src="imgs/left-arrow.png" width="30" height="30">  
           </button> */}
         <Image width="40px" height="40px" borderRadius="20px" />
-        <div class="info-block">
-          <p class="username">치치누나</p>
-          <p class="status">1시간 전에 활동</p>
-        </div>
-        <div class="call-box"></div>
-      </div>
+        <InfoBlock>
+          <p>치치누나</p>
+          <p>1시간 전에 활동</p>
+        </InfoBlock>
+        <CallBox></CallBox>
+      </Header>
 
-      <div id="chat-box">
-        <div class=" bubble friend-bubble">자니?</div>
-        <div class=" bubble friend-bubble">자나보네..ㅎ</div>
-        <div class=" bubble friend-bubble">잘 자 :)</div>
-        {ChatMessage.map((chat, idx) => {
-          return <div class=" bubble my-bubble">{chat}</div>;
+      <ChatBox>
+        {ChatInfo.map((chat, idx) => {
+          return (
+            <div key={idx}>
+              {chat.userName === NickName ? (
+                <BubbleBox user="my">
+                  <p>{chat.modifiedAt}</p>
+                  <Bubble user="my">{chat.message} </Bubble>
+                </BubbleBox>
+              ) : (
+                <BubbleBox user="friend">
+                  <Bubble user="friend">{chat.message}</Bubble>
+                  <p>{chat.modifiedAt}</p>
+                </BubbleBox>
+              )}
+            </div>
+          );
         })}
-      </div>
-
-      <div class="footer">
-        <textarea
-          onKeyPress={SendMessageByEnter}
-          id="input"
-          placeholder="메시지를 입력하세요..."
-          autofocus="true"
-          onChange={ChangeMessage}
-          value={message}
-        ></textarea>
-        <button id="send" type="summit" onClick={SendMessage}>
-          <Send class="sendBtn" />
-        </button>
-      </div>
-    </div>
+      </ChatBox>
+    </ChatWrap>
   );
 };
 
+const ChatWrap = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 540px;
+  background-color: #ffffff;
+  overflow: auto;
+`;
+
+const Header = styled.div`
+  display: flex;
+  height: 60px;
+  padding: 12px 5px 8px;
+  align-items: center;
+  box-shadow: 0 0 1px #999999;
+`;
+
+const InfoBlock = styled.div`
+  margin-left: 12px;
+  margin-right: 95px;
+  p {
+    :nth-child(1) {
+      margin: 0 0 3px;
+      font-size: 16px;
+    }
+    :nth-child(2) {
+      margin: 0;
+      font-size: 12px;
+      color: #999999;
+    }
+  }
+`;
+
+const CallBox = styled.div`
+  display: flex;
+  width: 60px;
+  justify-content: space-between;
+`;
+
+const ChatBox = styled.div`
+  height: 400px;
+  padding: 12px 10px;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BubbleBox = styled.div`
+  margin: 5px 0px;
+  display: flex;
+  ${(props) =>
+    props.user === "my"
+      ? `
+      justify-content:flex-end;
+    `
+      : ``}
+  p {
+    display: flex;
+    font-size: 10px;
+    margin: 10px 5px;
+    align-items: end;
+  }
+`;
+
+const Bubble = styled.div`
+  margin: 5px 0;
+  display: inline-block;
+  max-width: 300px;
+  font-size: 14px;
+  position: relative;
+  ${(props) =>
+    props.user === "my"
+      ? `
+      background-color: #cbcf52;
+      border-radius: 14px 14px 14px 0;
+      padding: 7px 15px 7px 15px;
+      float: right;
+      clear: both;
+      color: #000000;`
+      : `
+      background-color: #fbd986;
+      border-radius: 14px 14px 14px 0;
+      padding: 7px 15px 7px 15px;
+      float: left;
+      clear: both;
+      color: #000000;`}
+`;
 export default ChatTest;

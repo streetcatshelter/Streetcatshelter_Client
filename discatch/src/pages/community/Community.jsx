5f0d8b7d5d047f3e06da57534fbe3924ecb8a0a5
sunm-in <sunm309@naver.com>
2021-11-24 16:55:46 +0900

    🔨 Spinner 적용

--- discatch/src/pages/community/Community.jsx
+++ discatch/src/pages/community/Community.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 // COMPONENTS
-import { CommunityCategoryCard, Template } from "../../components";
+import { CommunityCategoryCard, Template, Spinner } from "../../components";
 
 // ELEMENTS
 import { Grid } from "../../elements/index";
@@ -17,6 +17,7 @@ import Community2 from "../../styles/images/Community2.svg";
 import Community3 from "../../styles/images/Community3.svg";
 
 const Community = (props) => {
+  const isLoaded = useSelector((state) => state.mypage.isLoaded);
   const village = useSelector((state) => state.map.keywordList[0]);
 
   const requestLocationInfo = () => {
@@ -25,63 +26,67 @@ const Community = (props) => {
   };
 
   return (
-    <Template props={props}>
-      {village !== undefined ? (
-        <Grid margin="10vh 0 0 0">
-          <CommunityCategoryCard
-            src={Community1}
-            clickEvent={() => history.push(`/community/${village}/catinfo`)}
-            width="100px"
-            height="100px"
-            title="고양이 정보글!"
-            subtitle="고양이는 집사를 좋아해요~"
-          />
-          <CommunityCategoryCard
-            src={Community2}
-            clickEvent={() => history.push(`/community/${village}/gathering`)}
-            width="100px"
-            height="100px"
-            title={`${village} 동네 모임`}
-            subtitle="동네 고양이에 대해 얘기 나눠봐요!"
-          />
-          <CommunityCategoryCard
-            src={Community3}
-            clickEvent={() => history.push(`/community/${village}/sharing`)}
-            width="100px"
-            height="100px"
-            title={`${village} 고양이 물건 나눔`}
-            subtitle="고양이를 위한 물건을 나눠봐요!"
-          />
-        </Grid>
-      ) : (
-        <Grid margin="10vh 0 0 0">
-          <CommunityCategoryCard
-            src={Community1}
-            clickEvent={() => requestLocationInfo()}
-            width="100px"
-            height="100px"
-            title="고양이 정보글!"
-            subtitle="고양이는 츄르를 좋아해요~"
-          />
-          <CommunityCategoryCard
-            src={Community2}
-            clickEvent={() => requestLocationInfo()}
-            width="100px"
-            height="100px"
-            title={`${village} 동네 모임`}
-            subtitle="동네 고양이에 대해 얘기 나눠봐요!"
-          />
-          <CommunityCategoryCard
-            src={Community3}
-            clickEvent={() => requestLocationInfo()}
-            width="100px"
-            height="100px"
-            title={`${village} 고양이 물건 나눔`}
-            subtitle="고양이를 위한 물건을 나눠봐요!"
-          />
-        </Grid>
-      )}
-    </Template>
+    <>
+      <Spinner visible={isLoaded} />
+      <Template props={props}>
+        {village !== undefined ? (
+          <Grid margin="10vh 0 0 0">
+            <CommunityCategoryCard
+              src={Community1}
+              clickEvent={() => history.push(`/community/${village}/catinfo`)}
+              width="100px"
+              height="100px"
+              title="고양이 정보글!"
+              subtitle="고양이는 집사를 좋아해요~"
+            />
+
+            <CommunityCategoryCard
+              src={Community2}
+              clickEvent={() => history.push(`/community/${village}/gathering`)}
+              width="100px"
+              height="100px"
+              title={`${village} 동네 모임`}
+              subtitle="동네 고양이에 대해 얘기 나눠봐요!"
+            />
+            <CommunityCategoryCard
+              src={Community3}
+              clickEvent={() => history.push(`/community/${village}/sharing`)}
+              width="100px"
+              height="100px"
+              title={`${village} 고양이 물건 나눔`}
+              subtitle="고양이를 위한 물건을 나눠봐요!"
+            />
+          </Grid>
+        ) : (
+          <Grid margin="10vh 0 0 0">
+            <CommunityCategoryCard
+              src={Community1}
+              clickEvent={() => requestLocationInfo()}
+              width="100px"
+              height="100px"
+              title="고양이 정보글!"
+              subtitle="고양이는 츄르를 좋아해요~"
+            />
+            <CommunityCategoryCard
+              src={Community2}
+              clickEvent={() => requestLocationInfo()}
+              width="100px"
+              height="100px"
+              title={`${village} 동네 모임`}
+              subtitle="동네 고양이에 대해 얘기 나눠봐요!"
+            />
+            <CommunityCategoryCard
+              src={Community3}
+              clickEvent={() => requestLocationInfo()}
+              width="100px"
+              height="100px"
+              title={`${village} 고양이 물건 나눔`}
+              subtitle="고양이를 위한 물건을 나눠봐요!"
+            />
+          </Grid>
+        )}
+      </Template>
+    </>
   );
 };
 

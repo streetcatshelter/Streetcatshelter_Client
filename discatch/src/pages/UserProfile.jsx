63f95f1e6ddbe343ae5dba1a50b00c96dcf76cbc
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-11-25 20:41:32 +0900

    🔨 유저인포 추가

---
+++ discatch/src/pages/UserProfile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Template } from "../components";
+
+const UserProfile = (props) => {
+  return <Template props={props}></Template>;
+};
+
+export default UserProfile;

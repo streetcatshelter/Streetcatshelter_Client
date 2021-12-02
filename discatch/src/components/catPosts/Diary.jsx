c8065e841138b17632d9e5a2da2ff977c5f67112
sunm-in <sunm309@naver.com>
2021-12-02 13:13:29 +0900

    🔨 다이어리 프로필 수정

--- discatch/src/components/catPosts/Diary.jsx
+++ discatch/src/components/catPosts/Diary.jsx
@@ -49,7 +49,7 @@ const Diary = ({ diary, location }) => {
       >
         <Grid display="flex" alignItems="center" width="auto">
           <Image
-            src={diary.profileImageUrl}
+            src={userProfile ? userProfile : diary.profileImageUrl}
             width="30px"
             height="30px"
             margin="0 5px 0 0"

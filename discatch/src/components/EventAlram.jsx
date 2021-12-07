fe7057db2c6f4408b14f4dc1a77623637c1f68a1
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-12-07 23:23:45 +0900

    🚧 이벤트 알람 작업중(실패)

--- discatch/src/components/EventAlram.jsx
+++ discatch/src/components/EventAlram.jsx
@@ -1,22 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const EventAlram = () => {
+  const [listening, setListening] = useState(false);
+  const [data, setData] = useState([]);
+  let eventSource = undefined;
+  const token = localStorage.getItem("token");
   useEffect(() => {
-    let es = new EventSource("http://52.78.241.50/alarm/init");
-    console.log(es);
-    es.onmessage = function (event) {
-      // 이벤트 설정이안된 기본 데이터 처리
+    if (!listening) {
+      eventSource = new EventSource(`http://52.78.241.50/alarm/init`, {
+        headers: { Authorization: "Bearer" + `${token}` },
+      });
+      console.log(eventSource);
+
+      eventSource.onopen = (event) => {
+        console.log("connection opened");
+      };
+
+      eventSource.onmessage = (event) => {
+        console.log("result", event.data);
+        setData((old) => [...old, event.data]);
+      };
+
+      eventSource.onerror = (event) => {
+        console.log(event.target.readyState);
+        if (event.target.readyState === EventSource.CLOSED) {
+          console.log("eventsource closed (" + event.target.readyState + ")");
+        }
+        eventSource.close();
+      };
+
+      setListening(true);
+    }
+
+    return () => {
+      eventSource.close();
+      console.log("eventsource closed");
     };
-    es.addEventListener(
-      "myevent",
-      function (e) {
-        // 'myevent' 이벤트의 데이터 처리
-      },
-      false
-    );
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        Received Data
+        {data.map((d) => (
+          <span key={d}>{d}</span>
+        ))}
+      </header>
+    </div>
+  );
 };
 
 export default EventAlram;

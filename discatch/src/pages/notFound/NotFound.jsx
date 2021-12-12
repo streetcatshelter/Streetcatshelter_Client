9a3098fd0ceee042498e8310da20edabe49ed7a7
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-12-12 16:59:53 +0900

    💄 notFound 페이지 뷰 수정

--- discatch/src/pages/notFound/NotFound.jsx
+++ discatch/src/pages/notFound/NotFound.jsx
@@ -3,12 +3,13 @@ import React from "react";
 
 // COMPONENTS
 import { Template } from "../../components";
-
+// STYLE
+import { css } from "styled-components";
 // ELEMENTS
-import { Button, Text } from "../../elements";
+import { Button, Text, Grid, Image } from "../../elements";
 
 // IMAGE
-import NotFoundCatImage from "../../styles/images/NotFoundCat.svg";
+import NotFoundCatImage from "../../styles/images/NotFoundCat.png";
 
 // REDUX
 import { history } from "../../redux/configureStore";
@@ -16,19 +17,65 @@ import { history } from "../../redux/configureStore";
 const NotFound = (props) => {
   return (
     <Template props={props}>
-      <Text size="30px" fontWeight="700">
-        {" "}
-        404 Error!!!
-      </Text>
-      <img src={NotFoundCatImage} alt={NotFoundCatImage} />
-      <Button
-        bgColor="yellow"
-        clickEvent={() => {
-          history.push("/");
+      <Grid
+        addstyle={() => {
+          return css`
+            width: 100%;
+            max-width: 420px;
+            height: 80vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+            @media screen and (max-height: 736px) {
+              height: 78vh;
+            }
+          `;
         }}
       >
-        <Text fontWeight="700">메인으로 돌아가기</Text>
-      </Button>
+        {" "}
+        <Text
+          size="20px"
+          fontWeight="700"
+          textAlign="center"
+          addstyle={() => {
+            return css`
+              background: #fdf0e7;
+              border-radius: 10px;
+            `;
+          }}
+        >
+          404 Error
+        </Text>
+        <Text size="45px" fontWeight="700" textAlign="center">
+          앗!
+        </Text>
+        <Text size="35px" fontWeight="700" textAlign="center">
+          잘못된 접근입니다.
+        </Text>
+        <Image
+          margin="20px auto"
+          width="200px"
+          height="200px"
+          src={NotFoundCatImage}
+          alt={NotFoundCatImage}
+        />
+        <Button
+          width="200px"
+          height="50px"
+          borderRadius="20px"
+          bgColor="yellow"
+          hoverBackground=" #cbcf52"
+          clickEvent={() => {
+            history.push("/");
+          }}
+        >
+          <Text fontWeight="700" size="20px">
+            홈으로 돌아가기
+          </Text>
+        </Button>
+      </Grid>
     </Template>
   );
 };

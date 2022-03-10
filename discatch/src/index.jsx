da7037aab2c865cb6d3351f794d730e6816a7f80
SooimChoi <85476908+leechoiswim1@users.noreply.github.com>
2022-03-10 16:51:28 +0900

    :hammer: reset-css 전체파일에 적용

--- discatch/src/index.jsx
+++ discatch/src/index.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./shared/App";
-import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "styles/GlobalStyle";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import store, { history } from "./redux/configureStore";
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <ConnectedRouter history={history}>
       <App />
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
-
-
-reportWebVitals();
